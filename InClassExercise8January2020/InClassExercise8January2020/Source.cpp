f3693f20e51141bb602842fbb97ddff9b2cd116f
roc.coughlin@gmail.com <roc.coughlin@gmail.com>
2020-01-08 18:14:01 -0700

    inital commit

---
+++ InClassExercise8January2020/InClassExercise8January2020/Source.cpp
@@ -0,0 +1,13 @@
+//Roc Coughlin
+//This is my own Work
+//In Class Exercise 1/8/2020
+#include<iostream>
+using namespace std;
+int main()
+{
+	int pounds;
+	cout << "please enter a number in pounds: ";
+	cin >> pounds;
+	cout << "the value in kilograms is " << pounds * 0.454 << ".\n";
+	return 0;
+}
