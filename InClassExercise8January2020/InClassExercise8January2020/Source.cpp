9c5b8b9b94ed335a58996622f07ee19df491cc4e
roc.coughlin@gmail.com <roc.coughlin@gmail.com>
2020-01-10 18:05:57 -0700

    will this let me change it?

--- InClassExercise8January2020/InClassExercise8January2020/Source.cpp
+++ InClassExercise8January2020/InClassExercise8January2020/Source.cpp
@@ -9,5 +9,25 @@ int main()
 	cout << "please enter a number in pounds: ";
 	cin >> pounds;
 	cout << "the value in kilograms is " << pounds * 0.454 << ".\n";
-	return 0;
+	
+	int side;
+	cout << "please enter the length of a side of a hexagon: ";
+	cin >> side;
+	float area = side * side * 3 * sqrt(3) * .5;
+	cout << "The aria of the hexagon is " << area << "\n";
+
+	int a;
+	int b;
+	cout << "please enter a number: ";
+	cin >> a;
+	cout << "please enter a second number: ";
+	cin >> b;
+	if (a > b) {
+		cout << a << " is biger then " << b << "\n";
+	}
+	else
+	{
+		cout << b << " is bigger then " << a << "\n";
+	}
+		return 0;
 }
